bac38e21c062589ef0fd394c06c0602a957196e6
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2018-01-12 15:51:01 -0200

    :tada: Initial structure
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

---
+++ src/edemocracia/settings.py
@@ -0,0 +1,130 @@
+from decouple import config, Csv
+import django.conf.global_settings as default
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# APPLICATION SETTINGS
+DEBUG = config('DEBUG', cast=bool, default=True)
+SECRET_KEY = config('SECRET_KEY', default='secret_key')
+
+RECAPTCHA_SITE_KEY = config(
+    'RECAPTCHA_SITE_KEY',
+    default='6LeqwioUAAAAAJQwLBKGmmpuazIQM6hEYYoFSTYW'
+)
+RECAPTCHA_PRIVATE_KEY = config(
+    'RECAPTCHA_PRIVATE_KEY',
+    default='6LeqwioUAAAAAHs4i1Zq4D_9kc1I-OL0TmaUowq3'
+)
+
+SITE_ID = 1
+ALLOWED_HOSTS = config('ALLOWED_HOSTS',
+                       cast=Csv(lambda x: x.strip().strip(',').strip()),
+                       default='*')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.' + config('DATABASE_ENGINE',
+                                                 default='sqlite3'),
+        'NAME': config('DATABASE_NAME',
+                       default=os.path.join(BASE_DIR, 'db.sqlite3')),
+        'USER': config('DATABASE_USER', default=''),
+        'PASSWORD': config('DATABASE_PASSWORD', default=''),
+        'HOST': config('DATABASE_HOST', default=''),
+        'PORT': config('DATABASE_PORT', default=''),
+    }
+}
+
+ROOT_URLCONF = 'edemocracia.urls'
+WSGI_APPLICATION = 'edemocracia.wsgi.application'
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+# PASSWORD VALIDATION
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.'
+                'UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.'
+                'MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.'
+                'CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.'
+                'NumericPasswordValidator',
+    },
+]
+
+
+# INTERNATIONALIZATION
+LANGUAGE_CODE = config('LANGUAGE_CODE', default='pt-br')
+TIME_ZONE = config('TIME_ZONE', default='America/Sao_Paulo')
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+LOCALE_PATHS = [
+    os.path.join(BASE_DIR, 'locale'),
+]
+
+LANGUAGES = (
+    ('en', 'English'),
+    ('pt-br', 'Brazilian Portuguese'),
+)
+
+# EMAIL SETTINGS
+EMAIL_HOST = config('EMAIL_HOST', default='localhost')
+EMAIL_PORT = config('EMAIL_PORT', cast=int, default=587)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool, default=True)
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='')
+
+# STATICFILES SETTINGS
+STATIC_URL = config('STATIC_URL', default='/static/')
+STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, 'public'))
+
+STATICFILES_FINDERS = default.STATICFILES_FINDERS + [
+    'npm.finders.NpmFinder',
+    'compressor.finders.CompressorFinder',
+]
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'core/static')]
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
