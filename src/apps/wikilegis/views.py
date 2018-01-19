0cff5e73cb7e56a5fb1b46a235aa2a6d4d4ae32e
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2018-01-19 11:59:33 -0200

    :sparkles: Add rewrites to wikilegis proxy view (#24)
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

--- src/apps/wikilegis/views.py
+++ src/apps/wikilegis/views.py
@@ -5,3 +5,13 @@ from django.conf import settings
 class WikilegisProxyView(EdemProxyView):
     upstream = settings.WIKILEGIS_UPSTREAM
     diazo_theme_template = 'diazo-base.html'
+    rewrite = (
+        (r'/wikilegis/admin/login/?$', '/admin/login'),
+        (r'/wikilegis/admin/login/\?([^=\n]+)\=([^&\n]+)$',
+         r'/admin/login/?next=\2'),
+        (r'/wikilegis/admin/logout/?$', '/admin/logout'),
+        (r'/wikilegis/login/?$', '/accounts/login'),
+        (r'/wikilegis/login/\?([^=\n]+)\=([^&\n]+)$',
+         r'/accounts/login/?next=\2'),
+        (r'/wikilegis/logout/?$', '/accounts/logout'),
+    )
