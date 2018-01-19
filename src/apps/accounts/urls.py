cec89150993ae60494ae4c37c87796750783b643
erivanio <erivanio.vanin@gmail.com>
2018-01-19 11:54:59 -0200

    :bug: fix name in register url

--- src/apps/accounts/urls.py
+++ src/apps/accounts/urls.py
@@ -5,6 +5,7 @@ from apps.accounts.views import CustomRegistrationView, ajax_login
 urlpatterns = [
     path('', include('registration.backends.default.urls')),
     path('', include('social_django.urls', namespace='social')),
-    path('ajax/signup/', CustomRegistrationView.as_view(), name='signup'),
+    path('ajax/signup/', CustomRegistrationView.as_view(),
+         name='registration_register'),
     path('ajax/login/', ajax_login, name="ajax_login"),
 ]
