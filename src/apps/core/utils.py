d8224ae14b8b342c1f21974d15c2d3dc8c7c3c8b
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2018-01-30 12:55:58 -0200

    Refactor get_user_data method
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

--- src/apps/core/utils.py
+++ src/apps/core/utils.py
@@ -1,10 +1,22 @@
 
 
 def get_user_data(user):
-    data = user.__dict__.copy()
-    data.pop('_state', None)
-    data.pop('password', None)
-    data.pop('last_login', None)
-    data.pop('date_joined', None)
-    data.pop('id', None)
-    return data
+    user_data = {
+        'name': user.first_name,
+        'email': user.email,
+        'country': user.profile.country,
+        'gender': user.profile.gender,
+        'uf': user.profile.uf,
+    }
+
+    if user.profile.birthdate:
+        user_data['birthdate'] = user.profile.birthdate.strftime("%x")
+    else:
+        user_data['birthdate'] = None
+
+    if user.profile.avatar:
+        user_data['avatar'] = user.profile.avatar.url
+    else:
+        user_data['avatar'] = None
+
+    return user_data
