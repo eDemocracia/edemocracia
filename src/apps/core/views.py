19a45868cd84a19bcc2da0734f298bef6c057a3f
erivanio <erivanio.vanin@gmail.com>
2018-01-30 11:45:31 -0200

    :ok_hand: add condition if apps doesnt enable

--- src/apps/core/views.py
+++ src/apps/core/views.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib.sites.shortcuts import get_current_site
 from revproxy.views import DiazoProxyView
 from django.shortcuts import render
@@ -24,17 +25,19 @@ class EdemProxyView(DiazoProxyView):
 
 def index(request):
     site = get_current_site(request)
-    pautas_url = site.domain + '/pautaparticipativa/api/v1/agenda/'
-    pautas_params = {'limit': '10', 'order_by': '-end_date'}
-    pautas_response = requests.get(pautas_url, params=pautas_params)
-    pautas_data = json.loads(pautas_response.text)
-    pautas = pautas_data['objects']
-    wikilegis_url = site.domain + '/wikilegis/api/v1/bill/'
-    wikilegis_params = {'limit': '10'}
-    wikilegis_response = requests.get(wikilegis_url, params=wikilegis_params)
-    wikilegis_data = json.loads(wikilegis_response.text)
-    bills = wikilegis_data['objects']
-    return render(request, 'index.html', {
-        'pautas': pautas,
-        'bills': bills
-    })
+    context = {}
+    if settings.PAUTAS_ENABLED:
+        pautas_url = site.domain + '/pautaparticipativa/api/v1/agenda/'
+        pautas_params = {'limit': '10', 'order_by': '-end_date'}
+        pautas_response = requests.get(pautas_url, params=pautas_params)
+        pautas_data = json.loads(pautas_response.text)
+        pautas = pautas_data['objects']
+        context['pautas'] = pautas
+    if settings.WIKILEGIS_ENABLED:
+        wikilegis_url = site.domain + '/wikilegis/api/v1/bill/'
+        wikilegis_params = {'limit': '10'}
+        wikilegis_response = requests.get(wikilegis_url, params=wikilegis_params)
+        wikilegis_data = json.loads(wikilegis_response.text)
+        bills = wikilegis_data['objects']
+        context['bills'] = bills
+    return render(request, 'index.html', context)
