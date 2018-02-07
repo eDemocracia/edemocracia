bd3e9de2e65c6db15936b6836228be9829f02392
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2018-02-07 17:27:02 -0200

    Update str_to_date method to cover more formats
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

--- src/apps/core/templatetags/edem_extras.py
+++ src/apps/core/templatetags/edem_extras.py
@@ -1,11 +1,13 @@
 from django import template
-from datetime import datetime
 from django.template import defaultfilters
+
+from apps.core import utils
+
 
 register = template.Library()
 
 
 @register.filter(name='str_to_date')
 def str_to_date(value):
-    result_date = datetime.strptime(value, "%Y-%m-%d").date()
-    return defaultfilters.date(result_date, "SHORT_DATE_FORMAT")
+    return defaultfilters.date(utils.str_to_date(value),
+                               "SHORT_DATE_FORMAT")
--- src/apps/core/utils.py
+++ src/apps/core/utils.py
@@ -1,3 +1,30 @@
+from datetime import datetime
+import pytz
+
+
+def str_to_date(value):
+    final_value = None
+    date_formats = {
+        '%d/%m/%Y': 'date',
+        '%d/%m/%Y %H:%M:%S': None,
+        '%d/%m/%Y %H:%M': None,
+        '%H:%M': 'time',
+        '%H:%M:%S': 'time',
+        '%Y-%m-%dT%H:%M:%S.%fZ': 'date',
+    }
+    for date_format in date_formats.keys():
+        try:
+            final_value = datetime.strptime(value, date_format)
+            tz = pytz.timezone('America/Sao_Paulo')
+            final_value = tz.localize(final_value)
+            if date_formats[date_format]:
+                get_date_type = getattr(final_value,
+                                        date_formats[date_format])
+                final_value = get_date_type()
+            break
+        except (ValueError, TypeError):
+            final_value = value.strip() if value else None
+    return final_value
 
 
 def get_user_data(user):
