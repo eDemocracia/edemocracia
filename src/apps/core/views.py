213e17abe8a6070fc1f8b9ddf486058ce6651aa2
Erivânio Vasconcelos <erivanio.vanin@gmail.com>
2018-02-19 15:24:09 -0300

    :recycle: Refactor/get index data (#45)
    * :recycle: create function to get wikilegis data
    
    * :recycle: create function to get pautas data
    
    * :recycle: create function to get audiencias data

--- src/apps/core/views.py
+++ src/apps/core/views.py
@@ -1,11 +1,12 @@
 from django.conf import settings
 from revproxy.views import DiazoProxyView
 from django.shortcuts import render
-import requests
 import json
-from datetime import datetime, timedelta
 
 from apps.discourse.data import get_discourse_index_data
+from apps.wikilegis.data import get_wikilegis_index_data
+from apps.pautas.data import get_pautas_index_data
+from apps.audiencias.data import get_audiencias_index_data
 
 
 class EdemProxyView(DiazoProxyView):
@@ -28,52 +29,19 @@ class EdemProxyView(DiazoProxyView):
 def index(request):
     context = {}
     if settings.PAUTAS_ENABLED:
-        pautas_url = settings.PAUTAS_UPSTREAM + '/api/v1/agenda/'
-        pautas_params = {'limit': '10', 'order_by': '-end_date'}
-        pautas_response = requests.get(pautas_url, params=pautas_params)
-        pautas = pautas_response.json()['objects']
-        context['pautas'] = pautas
+        context['pautas'] = get_pautas_index_data()
+
     if settings.WIKILEGIS_ENABLED:
-        wikilegis_url = settings.WIKILEGIS_UPSTREAM + '/api/v1/bill/'
-        wikilegis_params = {'limit': '10'}
-        wikilegis_response = requests.get(wikilegis_url,
-                                          params=wikilegis_params)
-        bills = wikilegis_response.json()['objects']
-        context['bills'] = bills
+        context['bills'] = get_wikilegis_index_data()
 
     if settings.DISCOURSE_ENABLED:
         context['topics'] = get_discourse_index_data()
 
     if settings.AUDIENCIAS_ENABLED:
-        audiencias_url = settings.AUDIENCIAS_UPSTREAM + '/api/room/'
-        today = datetime.today().strftime('%Y-%m-%d')
-        tomorrow = (datetime.today() + timedelta(1)).strftime('%Y-%m-%d')
-        today_params = {'date__gte': today, 'date__lt': tomorrow,
-                        'is_visible': 'True', 'ordering': '-date'}
-        today_response = requests.get(
-            audiencias_url, params=today_params)
-        today_rooms = today_response.json()['results']
-        live_rooms = [x for x in today_rooms if x['youtube_status'] == 1]
-        history_rooms = [x for x in today_rooms if x['youtube_status'] == 2]
-        agenda_rooms = [x for x in today_rooms if x['youtube_status'] == 0]
+        rooms = get_audiencias_index_data()
 
-        if len(today_rooms) == 0:
-            history_params = {
-                'youtube_status': 2, 'ordering': '-date'}
-            history_response = requests.get(
-                audiencias_url, params=history_params)
-            history_rooms = history_response.json()['results'][:10]
-        elif len(today_rooms) < 10:
-            diff_cards = 10 - len(today_rooms)
-            agenda_params = {
-                'date__gte': today, 'youtube_status': 0, 'ordering': 'date'}
-            agenda_response = requests.get(
-                audiencias_url, params=agenda_params)
-            extra_agenda_rooms = agenda_response.json()['results'][:diff_cards]
-            agenda_rooms = agenda_rooms + extra_agenda_rooms
-
-        context['history_rooms'] = history_rooms
-        context['agenda_rooms'] = agenda_rooms
-        context['live_rooms'] = live_rooms
+        context['history_rooms'] = rooms['history_rooms']
+        context['agenda_rooms'] = rooms['agenda_rooms']
+        context['live_rooms'] = rooms['live_rooms']
 
     return render(request, 'index.html', context)
