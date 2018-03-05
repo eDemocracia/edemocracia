e77671d6b326c7d213708313bc80c1dae6e45667
Erivânio Vasconcelos <erivanio.vanin@gmail.com>
2018-03-05 16:35:22 -0300

    Fix/bug fixes (#47)
    * :bug: replace username if None
    
    * :bug: fix pipelines order
    
    * :speech_balloon: add admin title
    
    * :bug: add email in required fields in user admin

--- src/edemocracia/settings.py
+++ src/edemocracia/settings.py
@@ -127,13 +127,13 @@ SOCIAL_AUTH_PIPELINE = (
     'social_core.pipeline.social_auth.social_details',
     'social_core.pipeline.social_auth.social_uid',
     'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.social_auth.associate_by_email',
     'social_core.pipeline.user.get_username',
     'social_core.pipeline.user.create_user',
     'apps.accounts.pipeline.save_profile',
     'social_core.pipeline.social_auth.associate_user',
     'social_core.pipeline.social_auth.load_extra_data',
     'social_core.pipeline.user.user_details',
-    'social_core.pipeline.social_auth.associate_by_email',
 )
 
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = config('SOCIAL_AUTH_GOOGLE_OAUTH2_KEY',
