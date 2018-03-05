de2b4e50f4fcd591f4d9b10610758251c69d041b
erivanio <erivanio.vanin@gmail.com>
2018-03-05 16:32:55 -0300

    :bug: add email in required fields in user admin

--- src/apps/accounts/admin.py
+++ src/apps/accounts/admin.py
@@ -1,9 +1,25 @@
 from django.utils.translation import ugettext_lazy as _
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth.models import User
 
 from apps.accounts.models import UserProfile
+
+
+class EmailRequiredMixin(object):
+    def __init__(self, *args, **kwargs):
+        super(EmailRequiredMixin, self).__init__(*args, **kwargs)
+        # make user email field required
+        self.fields['email'].required = True
+
+
+class CustomUserCreationForm(EmailRequiredMixin, UserCreationForm):
+    pass
+
+
+class CustomUserChangeForm(EmailRequiredMixin, UserChangeForm):
+    pass
 
 
 class ProfileInline(admin.StackedInline):
@@ -14,6 +30,11 @@ class ProfileInline(admin.StackedInline):
 
 
 class CustomUserAdmin(UserAdmin):
+    form = CustomUserChangeForm
+    add_form = CustomUserCreationForm
+    add_fieldsets = ((None, {'fields': ('username', 'email',
+                                        'password1', 'password2'),
+                             'classes': ('wide',)}),)
     inlines = (ProfileInline, )
 
     def get_inline_instances(self, request, obj=None):
