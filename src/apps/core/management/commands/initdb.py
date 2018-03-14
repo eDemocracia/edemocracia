cb566c34553eadd8527ee121726406c091833af3
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2018-03-14 16:05:37 -0300

    Updates to production docker (#48)
    * Update edem-navigation submodule
    
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>
    
    * Add script to create admin and update site
    
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>
    
    * Update docker-composes
    
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>
    
    * Remove unused method on initdb command
    
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

---
+++ src/apps/core/management/commands/initdb.py
@@ -0,0 +1,35 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+from django.contrib.sites.models import Site
+import re
+import os
+import sys
+
+
+User = get_user_model()
+
+
+class Command(BaseCommand):
+    help = 'Create admin and update site name'
+
+    def handle(self, *args, **options):
+        self.create_admin()
+
+    def create_admin(self):
+        admin_email = os.environ.get('ADMIN_EMAIL', None)
+        admin_username = os.environ.get('ADMIN_USERNAME', None)
+        admin_passwd = os.environ.get('ADMIN_PASSWORD', None)
+
+        if None not in [admin_email, admin_passwd, admin_username]:
+            print('Creating superuser...')
+            user = User.objects.get_or_create(email=admin_email,
+                                              username=admin_username)[0]
+            user.set_password(admin_passwd)
+            user.is_superuser = True
+            user.is_staff = True
+            user.save()
+            print('Done!')
+        else:
+            print('Missing ADMIN_EMAIL, ADMIN_USERNAME or ADMIN_PASSWORD '
+                  'environment variable.')
+            sys.exit(1)
