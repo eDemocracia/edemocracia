e6d6d1537871e6efa9bdeec540f1c3574a910d3a
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2019-07-24 15:08:49 -0300

    Add date to url
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

--- src/static/js/lightpick.js
+++ src/static/js/lightpick.js
@@ -1,13 +1,38 @@
 var picker = new Lightpick({
   lang: 'auto',
-  field: document.getElementById('datepicker'),
-  singleDate: false,
+  field: document.getElementById('startDate'),
+  secondField: document.getElementById('endDate'),
+  singleDate: true,
+  repipck: true,
   parentEl: '.page-statistics > .menu > .wrapper > .date',
   minDate: moment().startOf('month').add(7, 'day'),
   maxDate: moment().endOf('month').subtract(7, 'day'),
-  onSelect: function(start, end){
-    var str = '';
-    str += start ? start.format('Do MMMM YYYY') + ' to ' : '';
-    str += end ? end.format('Do MMMM YYYY') : '...';
-  }
 });
+
+$('.JS-updateDate').click(function() {
+  var startDate = $('#startDate').val();
+  var endDate = $('#endDate').val();
+
+  var url = new URL(location);
+  if (startDate) {
+    if (url.searchParams.has('startDate')) {
+      url.searchParams.set('startDate', startDate);
+    } else {
+      url.searchParams.append('startDate', startDate);
+    }
+  } else {
+    url.searchParams.delete('startDate');
+  }
+
+  if (endDate) {
+    if (url.searchParams.has('endDate')) {
+      url.searchParams.set('endDate', endDate);
+    } else {
+      url.searchParams.append('endDate', endDate);
+    }
+  } else {
+    url.searchParams.delete('endDate');
+  }
+
+  window.location = url;
+})
