477740410307604039f3764dae13d212cd023b70
Erivânio Vasconcelos <erivanio.vanin@gmail.com>
2021-04-23 14:36:08 -0300

    add and configure  celery (#113)

--- src/edemocracia/settings.py
+++ src/edemocracia/settings.py
@@ -59,6 +59,9 @@ INSTALLED_APPS = [
     'compressor_toolkit',
     'widget_tweaks',
     'corsheaders',
+    'celery',
+    'django_celery_beat',
+    'django_celery_results',
 
     'apps.core',
     'apps.wikilegis',
@@ -368,3 +371,14 @@ OLARK_ID = config('OLARK_ID', default=None)
 # CORS
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_URLS_REGEX = r'^.*/api/.*$'
+
+# CELERY related settings
+REDIS_SERVER = config('REDIS_SERVER', default='redis://localhost:6379/0')
+CELERY_BROKER_URL = REDIS_SERVER
+CELERY_RESULT_BACKEND = 'django_celery_results.backends.DatabaseBackend'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = TIME_ZONE
+CELERY_TASK_TRACK_STARTED = True
+CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
